1a3544b5de2928589a93315f3b7e6e69e6bfa5b4
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-06-12 07:58:33 +0000

    AXIOS added

--- src/app.js
+++ src/app.js
@@ -1,25 +1,33 @@
-// "https://swapi.dev/api/people/1"
+// // "https://swapi.dev/api/people/1"
 
-const req = new XMLHttpRequest();
+// const req = new XMLHttpRequest();
 
-req.onload = function(){
-    console.log("It loaded");
-    const data = JSON.parse(this.responseText);
-    console.log(data);
+// req.onload = function(){
+//     console.log("It loaded");
+//     const data = JSON.parse(this.responseText);
+//     console.log(data);
+// }
+
+// req.onerror = function(){
+//     console.log("Error!");
+//     console.log(this);
+// }
+
+// req.open("GET", "https://swapi.dev/api/people/1");
+// req.send();
+
+// fetch("https://swapi.dev/api/people/1")
+// .then((res) => {
+//     console.log("RESOLVED",res);
+// })
+// .vatch((e) =>{
+//     console.log("ERROR",e);
+// })
+
+const getStarWarsPerson = async(id) =>{
+    const res = await axios.get(`https://swapi.dev/api/people/${id}`);
+    console.log(res.data);
 }
 
-req.onerror = function(){
-    console.log("Error!");
-    console.log(this);
-}
-
-req.open("GET", "https://swapi.dev/api/people/1");
-req.send();
-
-fetch("https://swapi.dev/api/people/1")
-.then((res) => {
-    console.log("RESOLVED",res);
-})
-.vatch((e) =>{
-    console.log("ERROR",e);
-})
+getStarWarsPerson(5);
+getStarWarsPerson(10);
